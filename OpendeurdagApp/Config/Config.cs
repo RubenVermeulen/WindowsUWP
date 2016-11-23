82db58b447f587a5e6ccf42450d3105e196e391d
xanderberkein <xander.berkein@gmail.com>
2016-11-23 12:18:40 +0100

    OpendeurdagApp toegevoegd - Layout

---
+++ OpendeurdagApp/Config/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpendeurdagClient.Config
+{
+    class Config
+    {
+        public static string BaseUrlApi => "http://localhost:4036/api/campuses";
+    }
+}
---
+++ OpendeurdagApp/Models/Degree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpendeurdagClient.Models
+{
+    public class Degree
+    {
+        public int DegreeId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
