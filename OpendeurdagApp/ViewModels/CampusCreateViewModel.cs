13f325976b75e04609fead879f36a4f021571939
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-11-30 11:32:39 +0100

    Adds show campusen + starts creating campus (WIP)

---
+++ OpendeurdagApp/ViewModels/CampusCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpendeurdagApp.ViewModels
+{
+    public class CampusCreateViewModel : Template10.Mvvm.ViewModelBase
+    {
+        private HttpClient Client { get; set; }
+    }
+}
