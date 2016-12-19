aff7a2e49a9c338acad6100c6433779c2e2301ef
xanderberkein <xander.berkein@gmail.com>
2016-12-19 18:51:25 +0100

    Add viewmodel for CampusTourPage

--- OpendeurdagApp/Views/CampusTourPage.xaml.cs
+++ OpendeurdagApp/Views/CampusTourPage.xaml.cs
@@ -28,34 +28,7 @@ namespace OpendeurdagApp.Views
         public CampusTourPage()
         {
             this.InitializeComponent();
-            //this.Loaded += MainPage_Loaded;
-            //carousel.ItemClick += Carousel_ItemClick;
         }
 
-        //private async void Carousel_ItemClick(object arg1, CarouselViewItemClickEventArgs arg2)
-        //{
-        //    MessageDialog md = new MessageDialog($"You have clicked {(arg2.ClickItem as ICarouselViewItemSource).Title} ;-)", "Wow");
-        //    await md.ShowAsync();
-        //}
-
-
-        //private void MainPage_Loaded(object sender, RoutedEventArgs e)
-        //{
-        //    carousel.ItemImageSource = new List<ICarouselViewItemSource>()
-        //    {
-        //         new CarouselItemSource() {
-        //             ImageSource ="https://img1.doubanio.com/view/photo/photo/public/p1547743259.jpg",
-        //             Title ="title1" },
-        //         new CarouselItemSource() {
-        //             ImageSource ="https://img1.doubanio.com/view/photo/photo/public/p2183422782.jpg",
-        //             Title ="title2" },
-        //         new CarouselItemSource() {
-        //             ImageSource ="https://img1.doubanio.com/view/photo/photo/public/p832662844.jpg",
-        //             Title ="title3" },
-        //         new CarouselItemSource() {
-        //             ImageSource ="https://img1.doubanio.com/view/photo/photo/public/p752907403.jpg",
-        //             Title ="title" },
-        //    };
-        //}
     }
 }
